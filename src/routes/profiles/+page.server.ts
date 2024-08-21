610bd26c6c864176167087c07ba819a5cd3cd8b1
Bravolakmedia <olakunlebravo@gmail.com>
2024-08-22 00:38:26 +0100

    Coding a CRUD application using Svelte in TSoc Cohort 2 Workshop

--- src/routes/profiles/+page.server.ts
+++ src/routes/profiles/+page.server.ts
@@ -64,21 +64,23 @@ async function seed() {
 /** @type {import('./$types').Actions} */
 export const actions = {
 	
-  // update: async ({ request }) => {
-  //   const data = await request.formData();
-  //   const db = createPool({ connectionString: POSTGRES_URL })
-  //   const client = await db.connect();
+ update: async ({ request }) => {
+  const data = await request.formData();
+   const db = createPool({ connectionString: POSTGRES_URL })
+   const client = await db.connect();
+    
 
-  //   const email = data.get('email');
-	// 	const name = data.get('name');
+    const id = data.get('id');
+     const email = data.get('email');
+	 	const name = data.get('name');
 
-  //   const updateUser = await client.sql`
-  //   UPDATE names
-  //   SET email = ${email}, name = ${name}
-  //   WHERE     ;`
+    const updateUser = await client.sql`
+     UPDATE names
+     SET id=${id}, email = ${email}, name = ${name}
+    WHERE  id = ${id};`
 	
-	// 	return { success: true };
-	// },
+	 	return { success: true };
+	 },
 
   delete: async ({ request }) => {
     const data = await request.formData();
@@ -99,12 +101,13 @@ export const actions = {
     const db = createPool({ connectionString: POSTGRES_URL })
     const client = await db.connect();
 
+    const id = data.get('id');
     const email = data.get('email');
 		const name = data.get('name');
 
     const createUser = await client.sql`
-      INSERT INTO names (name, email)
-      VALUES (${name}, ${email})
+      INSERT INTO names (id, name, email)
+      VALUES (${id}, ${name}, ${email})
       ON CONFLICT (email) DO NOTHING;
     `
     return { success: true };
